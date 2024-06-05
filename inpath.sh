2bdcd4849c228d6fc6ee09e73274ee6339a03f25
Ricky Mao <362136980@qq.com>
2024-06-05 17:35:30 +0800

    add inpath file

---
+++ inpath.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+in_path() {
+    cmd=$1 path=$2 result=1
+    oldIFS=$IFS IFS=":"
+
+    for directory in "$PATH" 
+    do 
+      if [ -x $directory/$cmd ] ; then 
+        result=0 
+      fi 
+    done 
+    IFS=$oldIFS
+    return $result
+}
+
+checkForCmdInPath() {
+    var=$1 
+    if [ "$var" != "" ] ; then 
+      if [ "${var:0:1}" = "/" ] ; then 
+        if [ ! -x $var ] ; then 
+          return 1
+        fi 
+      elif ! in_path $1 "$PATH" ; then 
+        return 2
+      fi 
+    fi 
+}
+
+if [ $# -ne 1 ] ; then 
+  echo "Usage: $0 command" >&2
+  exit 1
+fi 
+
+checkForCmdInPath "$1"
+case $? in 
+  0 ) echo "$1 found in PATH" ;;
+  1 ) echo "$1 not found or not executable" ;;
+  2 ) echo "$1 not found" ;;
+esac
+
+exit 0
