3c21978e10f8dd937d24022e44fca15dc114de9d
Ricky Mao <362136980@qq.com>
2024-06-05 23:46:45 +0800

    add valid-date.sh

---
+++ valid-date.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+monthNumToName()
+{
+    case $1 in 
+      1 ) month="Jan" ;; 2 ) month="Feb" ;;
+      * ) echo "unknown month value $1" >&2
+      exit 1
+    esac
+    return 0
+}
+
+if [ $# -ne 3 ] ; then 
+  echo "Usage: $0 month day year" >&2
+  exit 1
+fi
+
+if [ $3 -le 99 ] ; then 
+  echo "$0: expect a 4-digit value." >&2
+  exit 1
+fi
+
+if [ -z $(echo $1 | sed 's/[:digit:]//g') ] ; then
+  monthNumToName $1
+else
+  month="$(echo $1 | cut -c1 | tr '[:lower:]' '[:upper:]')"
+  month="$(echo $1 | cut -c2-3 | tr '[:upper:]' '[:lower:]')"
+fi
+
+echo $month $2 $3
+
+exit 0
