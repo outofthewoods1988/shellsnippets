eed58667d99df1c73e0942bccd8c7e3b951577c7
Ricky Mao <362136980@qq.com>
2024-06-07 23:47:33 +0800

    update todo

--- todo.sh
+++ todo.sh
@@ -1,39 +1,17 @@
 #!/bin/bash
 
-validint()
-{
-    number="$1"; min="$2"; max="$3"
-
-    if [ -z $number ] ; then
-      echo "enter a number"
-      return 1
-    fi
-
-    if [ "${number%${number#?}}" = "-" ] ; then
-      testvalue="${number#?}"
-    else
-      testvalue="$number"
-    fi
-
-    nodigit="$(echo $number | sed 's/[[:digit:]]//g')"
-
-    if [ ! -z $nodigit ] ; then
-      echo "enter value without decimal"
-      return 1
-    fi
-
-    if [ $number -lt $min ] ; then
-      echo "less than min"
-      return 1
-    fi
-
-    if [ $number -gt $max ] ; then
-      echo "greater than max"
-      return 1
-    fi
-
-}
-
-if validint $1 $2 $3 ; then
-  echo "enter valid value"
+if [ "$1" = "-p" ] ; then
+  precision="$2"
+  shift 2
+else
+  precision=2
 fi
+
+bc -q -l << EOF
+  scale="$precision"
+  $*
+  quit
+EOF
+
+exit 0
+
