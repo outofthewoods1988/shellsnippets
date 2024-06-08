b23352955794af80459537054f3a17a3eb3c0f8d
Ricky Mao <362136980@qq.com>
2024-06-08 23:49:10 +0800

    update todo

--- todo.sh
+++ todo.sh
@@ -3,7 +3,7 @@
 IFS=":"
 exec=0
 unexec=0
-for directory in "$PATH" ; do
+for directory in $PATH ; do
   if [ -d "$directory" ] ; then
     for cmd in "$directory"/* ; do
       if [ -x "$cmd" ] ; then
