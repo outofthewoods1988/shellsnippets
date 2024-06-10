98e8fd4258911aeca749b1754ce7bde352195f6a
Ricky Mao <362136980@qq.com>
2024-06-10 14:00:55 +0800

    update todo

--- todo.sh
+++ todo.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-. scriptbc.sh
+. scriptbc
 
-c=$1
 readablesize()
 {
   if [ $1 -ge 1048576 ] ; then
-    echo "$( scriptbc.sh -p 2 $1 / 1048576 )GB"
+    echo "$( scriptbc -p 2 $1 / 1048576 )GB"
   elif [ $1 -ge 1024 ] ; then
-    echo "$( scriptbc.sh -p 2 $1 / 1024 )MB"
+    echo "$( scriptbc -p 2 $1 / 1024 )MB"
   else
     echo "${1}KB"
   fi
 }
 
-a=$(readablesize $c)
-echo $
+a=$(readablesize $1)
+echo $a
