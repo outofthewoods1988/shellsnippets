1e1d4061c81615fc385eb4eaaa2d452be637e7a0
Ricky Mao <362136980@qq.com>
2024-06-10 15:49:05 +0800

    update todo

--- todo.sh
+++ todo.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
-scriptbc=$(which scriptebc)
+scriptbc=$(which scriptbc)
 
 readablesize()
 {
   if [ $1 -ge 1048576 ] ; then
-    echo "$( scriptbc -p 2 $1 / 1048576 )GB"
+    echo "$(scriptbc -p 2 $1 / 1048576)GB"
   elif [ $1 -ge 1024 ] ; then
-    echo "$( scriptbc -p 2 $1 / 1024 )MB"
+    echo "$(scriptbc -p 2 $1 / 1024)MB"
   else
     echo "${1}KB"
   fi
 }
 
-readablesize $1
+if [ $# -gt 1 ] ; then
+  echo "Usage:"
+  exit 1
+elif [ $# -eq 1 ] ; then
+  cd "$@"
+  if [ $? -ne 0 ] ; then
+    exit 1
+  fi
+fi
+
+for file in *
+do
+  if [ -d "$file" ] ; then
+    size=$(ls "$file" | wc -l |sed 's/[^[:digit:]]//g')
+    if [ $size -eq 1 ] ; then
+      echo "$file ($size entry)"
+    else
+      echo "$file ($size entries)"
+    fi
+  else
+    size=$(ls -sk "$file" | awk 'print $1')
+    echo "$file ($(readablesize $size))"
+  fi
+done |
+  sed '/ /^^^/g'
+  xargs -n 2
+  sed 's/^^^/ /g'
+  awk -F\| '{printf "%-39s %-39s\n", $1, $2}'
+
+exit 0
