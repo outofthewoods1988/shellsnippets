0a8a26496aa48d193157e6fadfc9ad547a032c89
Ricky Mao <362136980@qq.com>
2024-06-15 21:19:45 +0800

    fix users and pipe

--- gcp/gcp_find_orphaned_disks.sh
+++ gcp/gcp_find_orphaned_disks.sh
@@ -3,9 +3,9 @@
 filter="${1:-.*}"
 shift || :
 
-gcloud compute disks list  --format="table[no-heading](name, zone.basename(), type.basename(), sizeGb, user.join(','))" |
-grep -E  "$filter"
+gcloud compute disks list  --format="table[no-heading](name, zone.basename(), type.basename(), sizeGb, users.join(','))" |
+grep -E  "$filter" |
 while read name zone type size users ; do
   [ -n "$users" ] && continue
-    echo "disk $name in zone $zone is orpahned disk."
+  echo "disk $name in zone $zone is orpahned disk."
 done
